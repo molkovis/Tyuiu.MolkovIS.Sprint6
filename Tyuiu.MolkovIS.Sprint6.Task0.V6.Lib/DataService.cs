26c0d703d7782ca59fbf9471f25e99a4d694427c
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-11-24 18:06:06 +0500

    Task0

--- Tyuiu.MolkovIS.Sprint6.Task0.V6.Lib/DataService.cs
+++ Tyuiu.MolkovIS.Sprint6.Task0.V6.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.MolkovIS.Sprint6.Task0.V6.Lib
         {
             double underRoot = x * x + x;
             double root = Math.Sqrt(underRoot);
-            return Math.Round(((double)x)/root);
+            return Math.Round(x/root,3);
         }
     }
 }
